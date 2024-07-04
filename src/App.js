39eef055e6cef013277b67544d3b2754915d3984
Ajeesh Joshy <jishcem@gmail.com>
2024-07-04 10:00:27 +0530

    Add instructions for tasks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+// 1. Create a counter component - a + button and when clicked increases the value of the number displayed.
+// 2. Create a counter component with increment and decrement option.
+// 3. Create a counter component with increment and decrement and a number text box which would decide how much it has to increment or decrement.
+
 function App() {
   return (
     <div className="App">
